7f44b00989e19b6463a5b5ec19cbab3cf0698e38
Will-Emery <williamemery.b@gmail.com>
2023-11-08 12:07:10 -0700

    pushing

--- app.js
+++ app.js
@@ -16,7 +16,33 @@ app.listen(3000, () => {
 /********************
  * ROUTES
  *********************/
-//home route
+//default route
 app.get("/", (req, res) => {
-	res.render("index");
+	res.render("login");
+});
+
+//login route
+app.get("/login", (req, res) => {
+	res.render("login");
+});
+
+app.post("/login", (req, res) => {
+	// Implement your login logic here
+	// Check if the credentials are valid and redirect as needed
+
+	res.redirect("/home");
+
+	//create the user object
+	let ff_user_data = {
+		username: req.body.username,
+		//password: req.body.password,
+		goal: "none",
+		goal_cal: 0,
+		goal_protein: 0,
+		goal_carbs: 0,
+		goal_fat: 0,
+	};
+
+	//save the user object to local storage
+	localStorage.setItem("ff_user_data", JSON.stringify(ff_user_data));
 });
